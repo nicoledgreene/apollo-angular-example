9b9f2d827071943303f95447835e7fa38e406c74
Nicole Greene <nicolegilbert16@gmail.com>
2022-02-04 10:26:04 -0800

    client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-graphql';
+
+  constructor(apollo: Apollo) {
+    apollo.query({
+      query: gql`{
+        shows {
+          title
+          network
+          genre
+        }
+      }`
+    }).subscribe((data) => {
+      console.log(data);
+    })
+  }
 }
